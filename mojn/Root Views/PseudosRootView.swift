1b7373a81e593516b34e0737d18d0e2a4f5439ee
Casper Rogild Storm <casper+github@rogildstorm.com>
2019-03-04 12:28:53 +0100

    added icon

---
+++ mojn/Root Views/PseudosRootView.swift
@@ -0,0 +1,9 @@
+//
+//  PseudosRootView.swift
+//  mojn
+//
+//  Created by Casper Rogild Storm on 04/03/2019.
+//  Copyright © 2019 Casper Rogild Storm. All rights reserved.
+//
+
+import Foundation
