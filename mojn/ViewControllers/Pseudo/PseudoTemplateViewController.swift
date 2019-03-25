0bc0393f089b142be3ee034d818b52e67ac3708b
Casper Rogild Storm <casper+github@rogildstorm.com>
2019-03-25 13:37:10 +0100

    able to create pseudo

---
+++ mojn/ViewControllers/Pseudo/PseudoTemplateViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PseudoTemplateViewController.swift
+//  mojn
+//
+//  Created by Casper Rogild Storm on 25/03/2019.
+//  Copyright © 2019 Casper Rogild Storm. All rights reserved.
+//
+
+import Foundation
---
+++ mojn/ViewModels/Pseudo/PseudoTemplateViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  PseudoTemplateViewModel.swift
+//  mojn
+//
+//  Created by Casper Rogild Storm on 25/03/2019.
+//  Copyright © 2019 Casper Rogild Storm. All rights reserved.
+//
+
+import Foundation
